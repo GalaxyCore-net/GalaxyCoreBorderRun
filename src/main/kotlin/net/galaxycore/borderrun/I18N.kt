9ee1278c05bf6822538f966193bbf844e155ae75
Flo-Mit-H <flogamesg@gmail.com>
2022-05-26 20:25:59 +0200

    :globe_with_meridians: add missing i18n keys

--- src/main/kotlin/net/galaxycore/borderrun/I18N.kt
+++ src/main/kotlin/net/galaxycore/borderrun/I18N.kt
@@ -10,6 +10,8 @@ fun registerI18NDE() {
         i18nDE(key, value, prefix)
     }
 
+    i18n("nopermission", "§c✗§7 Du hast hierfür keine Rechte", true)
+
     i18n("phase.lobby.counter", "§eDas Spiel start in $time", true)
     i18n("phase.lobby.counter.actionbar", "§e$time", false)
     i18n("phase.lobby.actionbar", "§e(§a%current%§e/§a%of%§e)", true)
@@ -22,12 +24,17 @@ fun registerI18NDE() {
 
     i18n("phase.end.counter", "§eDer Server schließt in $time", true)
     i18n("phase.end.counter.actionbar", "§eDer Server schließt in $time", false)
+
+    i18n("command.start.success", "§e✓§7 Das Spiel wurde gestartet!", true)
+    i18n("command.start.failure", "§c✗§7 Das Spiel ist bereits gestartet!", true)
 }
 
 fun registerI18NEN() {
     fun i18n(key: String, value: String, prefix: Boolean) {
         i18nEN(key, value, prefix)
     }
+
+    i18n("nopermission", "§c✗§7 You don't have permission to do this", true)
 
     i18n("phase.lobby.counter", "§eStarting in $time", true)
     i18n("phase.lobby.counter.actionbar", "§e$time", false)
@@ -41,4 +48,7 @@ fun registerI18NEN() {
 
     i18n("phase.end.counter", "§eThe server closes in $time", true)
     i18n("phase.end.counter.actionbar", "§eThe server closes in $time", false)
+
+    i18n("command.start.success", "§e✓§7 Started the game!", true)
+    i18n("command.start.failure", "§c✗§7 The game is already started!", true)
 }
